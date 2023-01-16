2f64ae43e638836f3c037dfd6a7a9eb63f30e268
Rodrigo Martinez Tanchela <rmtanchela@gmail.com>
2023-01-16 17:04:34 -0300

    Refatoração

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot/src/main/java/br/com/rodrigo/controller/MathController.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot/src/main/java/br/com/rodrigo/controller/MathController.java
@@ -1,4 +1,4 @@
-package br.com.rodrigo;
+package br.com.rodrigo.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -7,82 +7,69 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rodrigo.converters.NumberConverter;
 import br.com.rodrigo.exceptions.UnsupportedMathOperationException;
+import br.com.rodrigo.math.SimpleMath;
 
 @RestController
 public class MathController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	
+	private SimpleMath math = new SimpleMath();
 
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sum(@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Plase set a numeric value!");
 		}
-		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+		return math.sum(NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sub(@PathVariable (value = "numberOne") String numberOne, 
 			@PathVariable(value = "numberTwo") String numberTwo) throws Exception{
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Plase set a numeric value!");
 		}
-		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+		return math.sub(NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double mult(@PathVariable (value = "numberOne") String numberOne, 
 			@PathVariable(value = "numberTwo") String numberTwo) throws Exception{
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Plase set a numeric value!");
 		}
-		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+		return math.mult(NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double div(@PathVariable (value = "numberOne") String numberOne, 
 			@PathVariable(value = "numberTwo") String numberTwo) throws Exception{
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Plase set a numeric value!");
 		}
-		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+		return math.div(NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value = "/raiz/{numberOne}", method = RequestMethod.GET)
 	public Double raiz(@PathVariable (value = "numberOne") String numberOne) throws Exception{
-		if (!isNumeric(numberOne)) {
+		if (!NumberConverter.isNumeric(numberOne)) {
 			throw new UnsupportedMathOperationException("Plase set a numeric value!");
 		}
-		return Math.sqrt(convertToDouble(numberOne));
+		return math.raiz(NumberConverter.convertToDouble(numberOne));
 	}
 	
 	@RequestMapping(value = "/media/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double media(@PathVariable (value = "numberOne") String numberOne, 
 			@PathVariable(value = "numberTwo") String numberTwo) throws Exception{
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Plase set a numeric value!");
 		}
-		return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+		return math.media(NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
 	}
 	
-
-	private Double convertToDouble(String strNumber) {
-		if(strNumber == null) {
-			return 0D;
-		}
-		String number = strNumber.replaceAll(",", ".");
-		return Double.parseDouble(number);
-	}
-
-	private boolean isNumeric(String strNumber) {
-		if(strNumber == null) {
-			return false;
-		}
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-	}
-
 }
---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot/src/main/java/br/com/rodrigo/math/SimpleMath.java
@@ -0,0 +1,29 @@
+package br.com.rodrigo.math;
+
+
+
+public class SimpleMath {
+	public Double sum(Double numberOne, Double numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	public Double sub( Double numberOne, Double numberTwo) {
+		return numberOne - numberTwo;
+	}	
+
+	public Double mult( Double numberOne,  Double numberTwo) {		
+		return numberOne * numberTwo;
+	}	
+
+	public Double div( Double numberOne,  Double numberTwo) {
+		return numberOne / numberTwo;
+	}	
+
+	public Double raiz(Double numberOne){
+		return Math.sqrt(numberOne);
+	}
+	
+	public Double media(Double numberOne, Double numberTwo) {		
+		return (numberOne + numberTwo) / 2;
+	}
+}
